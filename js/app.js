210244eb3922084ae2032709c1e7b4a4e8d841bd
romina2911 <romina.hp@gmail.com>
2018-01-08 14:41:19 -0500

    subiendo vista splash

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//Nuestra presentación se vera durante 3 segundos
+
+// $(document).ready(function() {
+//     setTimeout(function() {
+//       window.location.href = 'views/register.html';
+//     }, 3000);
+//   });
