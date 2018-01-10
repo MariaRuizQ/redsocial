652c9c9886b035bef9fa6126d10f6f76cfaf2332
delacruzPamela <delacruzlozano.p@gmail.com>
2018-01-09 23:18:16 -0500

    Validar registro

--- js/registry.js
+++ js/registry.js
@@ -3,15 +3,64 @@ $(document).ready(function() {
   $lastName = $('#last_name');
   $email = $('#email');
   $password = $('#password');
+  $button = $('#guardar');
 
-  
+  var validateName = false;
+  var validateLastname = false; 
+  var validateEmail = false;
+  var validatePassword = false; 
 
   /**  Validar todos los campos **/
+  function activeButton() {
+    if (validateName && validateLastname && validateEmail && validatePassword) {
+      $button.attr('disabled', false);
+      $button.removeClass('btn-disabled');
+      $button.addClass('btn-color');
+    }
+  }
 
+  function desactiveButton() {
+    $button.attr('disabled', true);
+    $button.removeClass('btn-color');
+    $button.addClass('btn-disabled');
+  } 
 
-  $('#guardar').on('click', function(event) {
-    event.preventDefault();
-    console.log($name.val());
+  $name.on('input', function() {
+    if ($(this).val().length >= 3) {
+      validateName = true;
+      activeButton(); 
+    } else {
+      desactiveButton(); 
+    }
+  });
+
+  $lastName.on('input', function() {
+    if ($(this).val().length >= 3) {
+      validateLastname = true;
+      activeButton(); 
+    } else {
+      desactiveButton(); 
+    }
+  });
+  $email.on('input', function(event) {
+    if ($(this).val().length >= 12) {
+      validateEmail = true;
+      activeButton();
+    } else {
+      desactiveButton();
+    }
+  });
+ 
+  $password.on('input', function() {
+    if ($(this).val().length >= 6) {
+      validatePassword = true;
+      activeButton(); 
+    } else {
+      desactiveButton();
+    }
+  });
+
+  $button.on('click', function() {
     firebase.database().ref('registro')
     .set({
         nombres: $name.val(),
