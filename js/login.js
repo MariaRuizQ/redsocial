c61f94588fff2db474d7a7a0d8844bcbc06ef915
lidiecitass <lid_26_90@hotmail.com>
2018-01-10 14:11:12 -0500

    implementacion de login

---
+++ js/login.js
@@ -0,0 +1,25 @@
+$( document ).ready(function(){ 
+	var $email = $('#email');
+	var $password = $('#password');
+
+	$email.on('input', function(){
+		firebase.database().ref('registro')
+		.on('child_added', function(s){
+		var user = s.val();
+		if(user.email == $email.val() && user.password == $password.val()){
+		  console.log("lalll");
+		  $('#login').attr('disabled',false);
+		}
+	})
+	});
+	$password.on('input', function(){
+		firebase.database().ref('registro')
+		.on('child_added', function(s){
+		var user = s.val();
+		if(user.email == $email.val() && user.password == $password.val()){
+		  console.log("lalll");
+		  $('#login').attr('disabled',false);
+		}
+	})
+	});
+})
